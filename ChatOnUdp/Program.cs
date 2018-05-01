c24729845e66c01590bc53c40dd2261ea82186c0
user <user@PC>
2018-05-02 00:58:03 +0600

    Add project files.

---
+++ ChatOnUdp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatOnUdp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Получаем данные, необходимые для соединения
+                Console.WriteLine("Укажите локальный порт");
+                Chat.localPort = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Укажите удаленный порт");
+                Chat.remotePort = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Укажите удаленный IP-адрес");
+                Chat.remoteIPAddress = IPAddress.Parse(Console.ReadLine());
+
+                // Создаем поток для прослушивания
+                Thread tRec = new Thread(new ThreadStart(Chat.Receiver));
+                tRec.Start();
+
+                while (true)
+                {
+                    Chat.Send(Console.ReadLine());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+            }
+        }
+    }
+}
